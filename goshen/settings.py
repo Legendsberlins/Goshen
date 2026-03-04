ebf8f096d31f6ad6099af360a67735f0357a47b7
Legendsberlins <122905699+Legendsberlins@users.noreply.github.com>
2026-03-04 03:32:35 +0100

    Settings.py debug update

--- goshen/settings.py
+++ goshen/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'false').lower() in ('1', 'true', 'yes')
+DEBUG = os.environ.get('DEBUG', 'true').lower() in ('1', 'true', 'yes')
 
 if not SECRET_KEY:
     if DEBUG:
